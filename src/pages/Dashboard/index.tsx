ae9b1c9102dbe2d157b97ca1072336efa81bb80d
David Richard <david.richard@acct.global>
2021-11-12 01:19:12 -0300

    Add component dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,21 @@
+// import { FoodsContainer } from './styles';
+
+import { useState } from "react";
+
+// interface DashboardProps {
+//     children: ReactNode;
+// }
+  
+const Dashboard = () => {
+
+	// const [foods, setFoods] = useState([])
+	// const [editingFood, setEditingFood] = useState({})
+	// const [modalOpen, setModalOpen] = useState(false)
+	// const [editModalOpen, setEditModalOpen] = useState(false)
+
+	return (
+			<h1>Teste dash</h1>
+	)
+}
+
+export default Dashboard;
